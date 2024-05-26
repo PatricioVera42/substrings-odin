3235353459a3f0f4ab7304f5cc0f2ce3a85aa717
patovera <verapatricio9@gmail.com>
2024-05-26 00:25:19 -0300

    restarted

--- substrings.rb
+++ substrings.rb
@@ -1,28 +1,4 @@
-def substrings(string, dictionary)
-    result
-    for word in 0...dictionary.length
-
-    end 
+def substrings(string,dictionary)
+    result = {}
 end
 
-
-
-dictionary = []
-
-puts "Enter your words for the dictionary (enter 0 to finish): "
-dictionary_inputs = gets.chomp.strip.downcase
-
-while true 
-    
-    dictionary.push(dictionary_inputs)
-    dictionary_inputs = gets.chomp.strip.downcase
-    
-    break if dictionary_inputs == "0"
-    
-end
-
-puts "Enter the word to calculate the substrings from: "
-string_input = gets.chomp.strip.downcase
-
-result = substrings(string_input, dictionary)
-puts "The possible substrings of #{string_input} from #{dictionary} are #{result}"
